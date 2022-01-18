bdaa44fd16c3b9ff535970c5935d38fa39ce3763
Michael-McLaren <michael.mclaren.mail@gmail.com>
2022-01-18 11:48:39 +0000

    finally got it working, also have made some plots for the presentation

--- ml_model/model_architectures.py
+++ ml_model/model_architectures.py
@@ -6,3 +6,5 @@ Created on Wed Jan 12 09:56:39 2022
 @author: s1740929
 """
 
+class FCCNetwork(nn.Module):
+    def __init__(self, input_shape, num_output_classes, num_filters, num_layers, use_bias=False):
